f5b902012402dfbb3a428f608e3633dc5789cd99
Ari Sweedler <ari@sweedler.com>
2019-02-11 13:20:52 -0800

    Made index load the clips page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
-import AboutPage from './about'
+import ClipsPage from './clips'
 
-//TODO redirect the URL as well? instead of showing the same thing?
-const IndexPage = () => (
-  <AboutPage />
+//TODO redirect the URL as well? instead of showing the same thing? hacky stuff here is lame :(
+const IndexPage = ( {data} ) => (
+  <ClipsPage data={data} />
 )
+
+/* TODO how can I redirect to Clips page instead of leaving the blank URL? */
+export const query = graphql`
+  query IndexPageQuery {
+    site {
+      siteMetadata {
+        clips {
+          imageURL
+          title
+          description
+          articleURL
+          genre
+          publication
+          importance
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
